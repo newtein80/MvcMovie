fdb0642c5d50499047f7b4ee17478e1cb5f3b5c5
ParkJongSoo <newtein@naver.com>
2018-10-30 14:17:45 +0900

    2018.10.30.설명추가.01

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -11,17 +11,43 @@ namespace MvcMovie.Controllers
     {
         // 
         // GET: /HelloWorld/
-
+        /// <summary>
+        /// URL에 "/HelloWorld/"를 추가해서 호출할 수 있는 HTTP GET 메서드임
+        /// 컨트롤러의 메서드는 (액션 메서드, Action Methods)
+        /// 문자열 같은 기본 형식(Primitive Types) 대신 IActionResult나 ActionResult로부터 파생된 클래스를 반환
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
+            //시스템에게 HTML을 반환하도록 요청했으며 그에 따라 시스템이 응답을 한 것입니다.
+            //MVC는 전달 받은 URL에 근거하여 컨트롤러 클래스를 (그리고 그 내부의 액션 메서드를) 호출
+            //Razor 기반의 뷰 템플릿은 .cshtml 이라는 파일 확장자
+            //View 개체를 반환
+            //return View(); 구문을 실행함으로써 브라우저로 전송할 응답을 뷰 템플릿 파일을 이용해서 렌더하도록 지시
+            //(사용해야 할 뷰 템플릿 파일의 이름을 명시적으로 지정하지 않았기 때문에, MVC는 기본적으로 /Views/HelloWorld 폴더에 위치한 Index.cshtml 뷰 파일을 사용)
+            //메뉴 레이아웃은 Views/Shared/_Layout.cshtml 파일에 구현
             return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
-
+        /// <summary>
+        /// URL에 "/HelloWorld/Welcome/"를 추가해서 호출할 수 있는 HTTP GET 메서드임
+        /// numTimes 매개변수에 아무런 값도 전달되지 않았을 경우 기본값으로 1이 사용되도록 이 매개변수에 C#의 선택적 매개변수(Optional-Parameter) 기능이 사용
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="numTimes"></param>
+        /// <returns></returns>
         public IActionResult Welcome(string name, int numTimes = 1)
         {
+            //악의적인 입력으로부터 응용 프로그램을 보호하기 위해서 HtmlEncoder.Default.Encode를 사용 가능
+            //응답을 생성하기 위해서 필요한 간단한 데이터를 컨트롤러에서 뷰로 전달
+            //뷰 템플릿에 필요한 동적 데이터(매개변수들)를 컨트롤러 내부에서 ViewData 사전에 설정한 다음, 이를 뷰 템플릿에서 접근
+            //ViewData 사전은 동적 개체로, 원하는 것은 무엇이든 설정할 수 있으며, 여러분이 ViewData 개체에 무언가 설정하기 전까지는 아무런 속성도 정의되어 있지 않습니다.
+            //MVC 모델 바인딩 시스템이 자동으로 브라우저 주소 표시줄의 쿼리 문자열에 포함된 명명된 매개변수들(name과 numTimes)을 액션 메서드의 매개변수에 매핑
+            //URL로부터 얻어진 데이터가 모델 바인더에 의해서 컨트롤러로 전달됩니다.
+            //컨트롤러는 이 데이터를 ViewData 사전에 정리해서 담고 이 개체를 다시 뷰로 전달합니다.
+            //마지막으로 뷰는 해당 데이터를 HTML로 렌더해서 이를 브라우저에 전달
             ViewData["Message"] = "Hello " + name;
             ViewData["NumTimes"] = numTimes;
 
--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -65,6 +65,39 @@ namespace MvcMovie
 
             app.UseAuthentication();
 
+            //MVC에 의해서 사용되는 기본 URL 라우팅 로직은 호출할 코드를 결정하기 위해서 다음과 같은 세그먼트 형식을 사용
+            // /[Controller]/[ActionName]/[Parameters]
+            //아무런 URL 세그먼트로 지정하지 않으면, 기본적으로 아래의 코드에 강조된 템플릿 라인에 정의된 대로 "Home" 컨트롤러의 "Index" 메서드가 지정
+            /*
+             * 첫 번째 URL 세그먼트는 실행할 컨트롤러 클래스를 결정합니다. 따라서 localhost:xxxx/HelloWorld는 HelloWorldController 클래스에 매핑
+             * URL 세그먼트의 두 번째 부분은 클래스의 액션 메서드를 결정
+             * localhost:xxxx/HelloWorld/Index는 HelloWorldController 클래스의 Index 메서드를 실행
+             * Index 메서드가 명시적으로 메서드 이름이 지정되지 않았을 경우 컨트롤러에서 호출되는 기본 메서드로 지정
+             * URL 세그먼트의 세 번째 부분(Parameters)은 라우트 데이터
+             * 
+             * http://localhost:xxxx/HelloWorld/Welcome로 이동해봅니다. 그러면 Welcome 메서드가 실행되어 문자열이 반환
+             * 
+             * id 매개변수 뒤에 지정된 ?는 (id?) 이 매개변수가 선택적 매개변수임을 의미
+             * */
+
+            /*
+             * 1. 상위 폴더 열기(Open Containing Folder)
+             * 2. 오른쪽 버튼으로 아무 폴더나 클릭한 다음, 여기서 명령 창 열기(Open command window here)를 선택
+             * 3. 명령 프롬프트에서 cd .. 명령을 실행해서 한 단계 위의 프로젝트 디렉터리로 이동
+             * 4. 명령 프롬프트에 다음 명령들을 차례대로 실행합니다
+             *    dotnet ef migrations add Initial
+             *    dotnet ef database update
+             * > 명령어 설명
+             * dotnet :
+             *         (.NET Core)은 크로스-플랫폼을 지원하는 .NET의 구현
+             * dotnet ef migrations add Initial :
+             *         명령은 Entity Framework .NET Core CLI의 마이그레이션 명령을 실행하고 첫 번째 마이그레이션을 생성합니다.
+             *         이 명령에서 "Initial" 대신 다른 인자 값을 이름으로 지정할 수도 있지만,
+             *         관례적으로 첫 번째 데이터베이스 마이그레이션에는 "Initial"이라는 이름을 지정하는 경우가 대부분입니다.
+             *         이 작업을 수행하면 데이터베이스에 Movie 테이블을 추가하는 (또는 드랍하는) 마이그레이션 명령이 구현된 Data/Migrations/2016<date-time>_Initial.cs 파일이 생성
+             * dotnet ef database update : 
+             *         명령은 방금 생성한 마이그레이션을 이용해서 데이터베이스를 갱신
+             * */
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
